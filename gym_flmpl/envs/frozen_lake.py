1f727f062edac63f341b1490124ccd4381dc376b
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2018-01-25 12:11:11 +0900

    [add] matplotlibで描画する機能を追加

--- gym_flmpl/envs/frozen_lake.py
+++ gym_flmpl/envs/frozen_lake.py
@@ -1,12 +1,20 @@
 # -*- coding:utf-8 -*-
 from gym.envs.toy_text import frozen_lake
+from gym_flmpl.envs.rendering import Viewer
 
 
 class FrozenLakeEnv(frozen_lake.FrozenLakeEnv):
-    metadata = {'render.modes': ['human', 'ansi', 'rgb_array']}
+    metadata = {
+        'render.modes': ['human', 'ansi', 'rgb_array', 'mpl']
+    }
 
-    def _render(self, mode='human', close=False):
-        if mode != 'rgb_array':
-            return super(FrozenLakeEnv, self)._render(mode, close)
+    def _render(self, mode='mpl', close=False):
+        if mode in ['rgb_array', 'mpl']:
+            if close:
+                return
+            if not hasattr(self, 'viewer'):
+                self.viewer = Viewer(self.desc)
+            r = self.viewer.render(self.s, self.lastaction, mode)
+            return r
         else:
-            raise NotImplementedError
+            return super(FrozenLakeEnv, self)._render(mode, close)
---
+++ gym_flmpl/envs/rendering.py
@@ -0,0 +1,92 @@
+# -*- coding:utf-8 -*-
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import numpy as np
+from gym.envs.toy_text import frozen_lake
+
+
+class Viewer(object):
+    FROZEN_COLOR = (0, 255, 255)    # cyan
+    HOLE_COLOR = (128, 128, 128)    # gray
+    ACITON_DICT = {
+        frozen_lake.LEFT: 'LEFT',
+        frozen_lake.DOWN: 'DOWN',
+        frozen_lake.RIGHT: 'RIGHT',
+        frozen_lake.UP: 'UP'
+    }
+
+    def __init__(self, maze):
+        self.__maze_canvas = None
+        self.__ax = None
+        self.__sg_dict = {}
+        self.__nrows = None
+        self.__ncols = None
+        self.__fig = None
+        self.__canvas = None
+        self.__initialize_maze_canvas(maze)
+
+    def __initialize_maze_canvas(self, maze):
+        start = np.argwhere(maze == 'S')[0]
+        goal = np.argwhere(maze == 'G')[0]
+        self.__sg_dict['S'] = start
+        self.__sg_dict['G'] = goal
+
+        self.__nrows, self.__ncols = maze.shape
+        canvas_shape = [self.__nrows, self.__ncols, 3]
+        self.__maze_canvas = np.zeros(canvas_shape, dtype=np.uint8)
+        self.__maze_canvas[:, :] = self.FROZEN_COLOR
+        self.__maze_canvas[maze == 'H'] = self.HOLE_COLOR
+        self.__fig = Figure()
+        self.__canvas = FigureCanvas(self.__fig)
+
+    def render(self, state, lastaction, mode='mpl'):
+        if lastaction is not None:
+            action_str = self.ACITON_DICT[lastaction]
+        else:
+            action_str = 'None'
+        if mode == 'mpl':
+            ax = plt.gca()
+        else:
+            ax = self.__fig.gca()
+        ax.cla()
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xticks(
+            np.arange(-0.5, self.__ncols - 0.5)
+        )
+        ax.set_yticks(
+            np.arange(-0.5, self.__nrows - 0.5)
+        )
+        ax.set_title('Last action: %s' % action_str, fontsize=18)
+        ax.grid(linestyle='dashed')
+        ax.imshow(self.__maze_canvas, interpolation='nearest')
+        self.__agent_location = np.array(
+            [state // self.__ncols, state % self.__ncols]
+        )
+        for text, location in self.__sg_dict.iteritems():
+            if not np.allclose(self.__agent_location, location):
+                y, x = location
+                ax.text(
+                    x, y, text,
+                    va='center',
+                    ha='center',
+                    fontsize=24
+                )
+        y, x = self.__agent_location
+        ax.text(
+            x, y, 'A',
+            va='center',
+            ha='center',
+            fontsize=24
+        )
+        if mode == 'rgb_array':
+            self.__canvas.draw()
+            width, height = self.__fig.get_size_inches() * self.__fig.get_dpi()
+            img = np.fromstring(
+                self.__canvas.tostring_rgb(), dtype='uint8'
+            )
+            img = img.reshape(int(height), int(width), 3)
+            return img
+        else:
+            return plt.pause(1e-6)
