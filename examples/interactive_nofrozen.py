2cd6ac0e5edf8d613e8dd920fb9ebc9a21d4e8b7
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2018-01-27 17:01:07 +0900

    [add] is_slipper=Falseの場合の例を追加

---
+++ examples/interactive_nofrozen.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import gym
+import gym_flmpl
+import matplotlib.pyplot as plt
+
+
+KEY_TO_ACTION = {
+    'left': 0,
+    'down': 1,
+    'right': 2,
+    'up': 3
+}
+
+
+def main():
+    env = gym_flmpl.envs.FrozenLakeEnv(is_slippery=False)
+
+    def get_action(event):
+        if event.key in KEY_TO_ACTION.keys():
+            act = KEY_TO_ACTION[event.key]
+            ob, reward, done, info = env.step(act)
+            env.render(mode='mpl')
+            print("Observation: {}".format(ob))
+            print("Reward: {}".format(reward))
+            print("Info: {}".format(info))
+            if done:
+                env.reset()
+                env.render(mode='mpl')
+                print("Done!")
+
+    fig = plt.figure()
+    fig.canvas.mpl_connect('key_press_event', get_action)
+    env.reset()
+    env.render(mode='mpl')
+    plt.pause(0)
+
+
+if __name__ == "__main__":
+    main()
